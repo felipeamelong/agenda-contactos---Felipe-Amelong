efb9fb04822dfe8a26aacb42942792db04a7b39c
Felipe Amelong <felipeamelong@gmail.com>
2025-10-19 19:21:35 -0300

    Configuración de página de detalles de contacto

--- src/app/pages/contact-details/contact-details.ts
+++ src/app/pages/contact-details/contact-details.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { ContactService } from '../../services/contact-service';
+import { ContactT } from '../../interfaces/contact-type';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-details',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './contact-details.html',
   styleUrl: './contact-details.scss'
 })
-export class ContactDetails {
-
+export class ContactDetails implements OnInit {
+  contactService = inject(ContactService);
+  id = input.required<string>(); 
+  contact: ContactT | undefined = undefined;
+  async ngOnInit() {
+    this.contact = await this.contactService.getContactsById(this.id());
+  }
 }
